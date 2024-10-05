e228b7972af7883bfdb0eb62b3a16f4d78654a9c
Furkan Çetintaş <66469657+furkancetintas@users.noreply.github.com>
2024-10-05 20:06:51 +0300

    reuired added!

--- src/components/SearchNotes/index.js
+++ src/components/SearchNotes/index.js
@@ -59,6 +59,7 @@ function SearchNotes() {
             <form>
               <select
                 onChange={(e) => dispatch(filterColorHandle(e.target.value))}
+                required
                 className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
               >
                 <option className={`bg-white w-full`} value={''}>
